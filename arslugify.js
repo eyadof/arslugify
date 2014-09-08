28012e69f9c2cb0f445bd0e53875bd058933b357
Eyad Arnabeh <eyadof@gmail.com>
2014-09-09 01:35:58 +0300

    init commit

---
+++ arslugify.js
@@ -0,0 +1,18 @@
+var XRegExp = require('xregexp').XRegExp;
+
+function dasherize(value){
+	dashedString = value.replace(/\s/g,'-');
+	return dashedString;
+}
+
+function slugify(value){
+	cleanReg = XRegExp('\\p{S}|\\p{P}','g');
+	cleanedString = XRegExp.replace(value,cleanReg,'');
+	cleanedString = cleanedString.trim();
+	cleanedString = cleanedString.toLowerCase();
+	slugifiedString = dasherize(cleanedString);
+
+	return slugifiedString; 
+}
+
+module.exports = slugify;
---
+++ test/test.js
@@ -0,0 +1,57 @@
+var ars = require('../arSlugify');
+
+describe('arSlugify',function(){
+	describe('only Arabic letters text',function(){
+		string = ars('نص عربي كثير الفائدة');
+		it('should return a string without whitespaces',function(next){
+			if(string.indexOf(' ') != -1)
+				next(new Error('whitespaces in the string'));
+			else
+				next()
+		})
+
+		it('should return a string with -',function(next){
+			if(string.indexOf('-') == -1)
+				next(new Error('no - in the string!'))
+			else
+				next()
+		})
+	})
+
+	describe('Arabic and English text',function(){
+		str2 = ars('نص عربي مع English')
+		it('should keep arabic letters',function(next){
+			if(str2.indexOf('ن') == -1){
+				next( new Error('Arabic letter not found!'))
+			}
+			else
+				next()
+		})
+
+		it('should keep english letters',function(next){
+			if(str2.indexOf('n') == -1){
+				next(new Error('English letter not found!'))
+			}
+			else
+				next()
+		})
+
+		it('should convert english letters to lower case',function(next){
+			if(str2.indexOf('E') != -1){
+				next(new Error('there is a letter in upper case'))
+			}
+			else
+				next()
+		})
+	})
+
+	describe('Other chars',function(){
+		str3 = ars('<>?/!@#$%^&*()_+=-;,.');
+		it('should remove any other chars',function(next){
+			if(str3.length > 0){
+				next(new Error('there is some strange chars !'))
+			}
+			next()
+		})
+	})
+})
